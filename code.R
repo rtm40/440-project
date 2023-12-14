f7728dc2b1d118c5819ecba087e743674994d27f
rtm40 <ryan.mitchell@duke.edu>
2023-12-14 15:14:36 -0500

    initial eda

--- code.R
+++ code.R
@@ -1,13 +1,15 @@
-```{r libraries-data, message = F, warning = F}
 # libraries and data
 library(tidyverse)
 library(scales)
 library(ggridges)
 library(patchwork)
+library(showtext)
+library(ggtext)
 bbdata <- read_csv("data/bbdata.csv")
-```
+font_add_google("STIX Two Text", "Times New Roman")
+showtext_auto()
 
-```{r}
+# data prep
 col4 <- c("#E06060", "#FFBFBF", "#E7F9FF", "#95D2EC")
 col5 <- c("#E06060", "#FFBFBF", "#E7F9FF", "#95D2EC", "#47ABD8")
 
@@ -33,25 +35,24 @@ bb <- bbdata |>
     ))
 
 bb$placement_bucket <- ordered(bb$placement_bucket,
-                                 levels = c("Early Out",
-                                            "Lower Middle",
-                                            "Upper Middle",
-                                            "Late Game",
-                                            "Winner"))
+                               levels = c("Early Out",
+                                          "Lower Middle",
+                                          "Upper Middle",
+                                          "Late Game",
+                                          "Winner"))
 
 bb$wins_bucket <- ordered(bb$wins_bucket,
-                                 levels = c("Bottom Quartile",
-                                            "Low Quartile",
-                                            "High Quartile",
-                                            "Top Quartile"))
+                          levels = c("Bottom Quartile",
+                                     "Low Quartile",
+                                     "High Quartile",
+                                     "Top Quartile"))
 
 bb$age_bucket <- ordered(bb$age_bucket,
-                                 levels = c("19-29",
-                                            "30-39",
-                                            "40+"))
-```
+                         levels = c("19-29",
+                                    "30-39",
+                                    "40+"))
 
-```{r}
+# stacked bar chart dataframes
 bb_bipoc_place <- bb |>
   group_by(BIPOC, placement_bucket) |>
   summarize(n = n()) |>
@@ -104,7 +105,8 @@ bipoc_place <-
   theme_void() +
   theme(axis.title.y = element_text(face = "bold"),
         axis.text.y = element_text(),
-        text = element_text(family = "Times New Roman"))
+        text = element_text(family = "Times New Roman", size = 14),
+        legend.text = element_text(size = 9))
 
 lgbt_place <-
   ggplot(bb_lgbt_place, aes(y = LGBT, x = freq, label = per, fill = placement_bucket)) +
@@ -116,7 +118,8 @@ lgbt_place <-
   theme_void() +
   theme(axis.title.y = element_text(face = "bold"),
         axis.text.y = element_text(),
-        text = element_text(family = "Times New Roman"))
+        text = element_text(family = "Times New Roman", size = 14),
+        legend.text = element_text(size = 9))
 
 gender_place <-
   ggplot(bb_gender_place, aes(y = gender, x = freq, label = per, fill = placement_bucket)) +
@@ -128,7 +131,8 @@ gender_place <-
   theme_void() +
   theme(axis.title.y = element_text(face = "bold"),
         axis.text.y = element_text(),
-        text = element_text(family = "Times New Roman"))
+        text = element_text(family = "Times New Roman", size = 14),
+        legend.text = element_text(size = 9))
 
 age_place <-
   ggplot(bb_age_place, aes(y = age_bucket, x = freq, label = per, fill = placement_bucket)) +
@@ -140,7 +144,8 @@ age_place <-
   theme_void() +
   theme(axis.title.y = element_text(face = "bold"),
         axis.text.y = element_text(),
-        text = element_text(family = "Times New Roman"))
+        text = element_text(family = "Times New Roman", size = 14),
+        legend.text = element_text(size = 9))
 
 gender_wins <-
   ggplot(bb_gender_wins, aes(y = gender, x = freq, label = per, fill = wins_bucket)) +
@@ -152,7 +157,7 @@ gender_wins <-
   theme_void() +
   theme(axis.title.y = element_text(face = "bold"),
         axis.text.y = element_text(),
-        text = element_text(family = "Times New Roman"))
+        text = element_text(family = "Times New Roman", size = 14))
 
 age_wins <-
   ggplot(bb_age_wins, aes(y = age_bucket, x = freq, label = per, fill = wins_bucket)) +
@@ -164,16 +169,13 @@ age_wins <-
   theme_void() +
   theme(axis.title.y = element_text(face = "bold"),
         axis.text.y = element_text(),
-        text = element_text(family = "Times New Roman"))
-```
+        text = element_text(family = "Times New Roman", size = 14))
 
-```{r}
-(bipoc_place / lgbt_place / gender_place / age_place) +
+# patchwork
+fig1 <- (bipoc_place / lgbt_place / gender_place / age_place) +
   plot_layout(guides = "collect", heights = c(1, 1, 1, 1.33)) &
   theme(legend.position = "top")
 
-(gender_wins / age_wins) +
+fig2 <- (gender_wins / age_wins) +
   plot_layout(guides = "collect", heights = c(1, 1.33)) &
   theme(legend.position = "top")
-```
-
